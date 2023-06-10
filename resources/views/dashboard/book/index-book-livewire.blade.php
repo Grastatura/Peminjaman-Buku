a3320057d079f10a72b03f6092a06c20558240bf
Grastatura <grastatura@gmail.com>
2023-06-10 22:37:52 +0700

    Update index-book-livewire.blade.php

--- resources/views/dashboard/book/index-book-livewire.blade.php
+++ resources/views/dashboard/book/index-book-livewire.blade.php
@@ -4,33 +4,32 @@
         <thead>
             <tr>
                 <th>No</th>
-                <th>Title Book</th>
-                <th>Date Published</th>
-                <th>Description</th>
-                <th>Category</th>
+                <th>Judul Buku</th>
+                <th>Penulis</th>
+                <th>Penerbit</th>
                 <th>Action</th>
             </tr>
         </thead>
         <tbody>
 
             @foreach ($allbook as $item)
-                <tr>
-                    <td>{{ $loop->index+1 }}</td>
-                    <td>{{ $item->name }}</td>
-                    <td>{{ $item->published_at }}</td>
-                    <td>{!! Str::limit(html_entity_decode(strip_tags($item->desc_book)), 20) !!}</td>
-                    <td>{{ $item->category->name_category }}</td>
-                    <td>
-                        <a href="/dashboard/book/{{ Crypt::encrypt($item->id) }}/edit" class="btn btn-primary"><i
-                                class="ri-edit-line"></i></a>
-                        <a href="/detail/{{ Crypt::encrypt($item->id) }}" type="button" wire:click="show('{{ Crypt::encrypt($item->id) }} }}')"
-                            class="btn btn-secondary"><i class="ri-eye-line"></i></a>
-                        <button type="button"
-                            onclick="confirm('Are you sure you want to delete this Author ? delete category same you delete book in auhtor this') || event.stopImmediatePropagation()"
-                            wire:click="destroy('{{ Crypt::encrypt($item->id) }} }}')" class="btn btn-success"><i
-                                class="ri-delete-bin-7-line"></i></button>
-                    </td>
-                </tr>
+            <tr>
+                <td>{{ $loop->index+1 }}</td>
+                <td>{{ $item->name }}</td>
+                <td>{{ $item->published_at }}</td>
+                <td>{{ $item->category->name_category }}</td>
+                <td>
+                    <a href="/dashboard/book/{{ Crypt::encrypt($item->id) }}/edit" class="btn btn-primary"><i
+                            class="ri-edit-line"></i></a>
+                    <a href="/detail/{{ Crypt::encrypt($item->id) }}" type="button"
+                        wire:click="show('{{ Crypt::encrypt($item->id) }} }}')" class="btn btn-secondary"><i
+                            class="ri-eye-line"></i></a>
+                    <button type="button"
+                        onclick="confirm('Are you sure you want to delete this Author ? delete category same you delete book in auhtor this') || event.stopImmediatePropagation()"
+                        wire:click="destroy('{{ Crypt::encrypt($item->id) }} }}')" class="btn btn-success"><i
+                            class="ri-delete-bin-7-line"></i></button>
+                </td>
+            </tr>
             @endforeach
         </tbody>
     </table>
