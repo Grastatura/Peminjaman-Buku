af153a2a6fde0f24593147a5a1ca33a699366258
Grastatura <grastatura@gmail.com>
2023-06-10 22:16:39 +0700

    Update side-bar-dashboard.blade.php

--- resources/views/components/side-bar-dashboard.blade.php
+++ resources/views/components/side-bar-dashboard.blade.php
@@ -44,7 +44,7 @@
         <li class="nav-item">
             <a class="nav-link @if (Request::is('dashboard/user*')) {{ 'active' }} @endif"" href=" /dashboard/user">
                 <span data-feather="users"></span>
-                Management User
+                Daftar Anggota
             </a>
         </li>
         <!-- <li class="nav-item">
