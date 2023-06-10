77638eebd24e6e2e80f66015335fcdc45068ff39
Grastatura <grastatura@gmail.com>
2023-06-10 22:08:38 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,11 +35,7 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
-                    {{ __('Register Your New Account') }}
-                </a>
-                @endif
+                (Route::has('password.request'))
 
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
